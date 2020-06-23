1f2e213b88f56d237e27ebba8fc98442a3a2bad1
Vincenzo Marcovecchio <55591572+VincenzoMarcovecchio@users.noreply.github.com>
2020-06-24 00:25:24 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var randomNumber2 = Math.floor((Math.random() * 6 + 1));
 
 // Change dice images
 document.getElementsByClassName("img1")[0].setAttribute("src", "images/dice" + randomNumber1 + ".png");
-document.getElementsByClassName("img2")[0].setAttribute("src", "images/dice" + randomNumber2 + ".png");
+document.getElementsByClassName("img2")[1].setAttribute("src", "images/dice" + randomNumber2 + ".png");
 
 // Change title
 if (randomNumber1 === randomNumber2) {
